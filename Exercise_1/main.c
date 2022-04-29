8ab00d56137e376c7c2b895a0575a268ea6d20da
Maxsuel Fernandes de Almeida <maxsuell.fernandes@gmail.com>
2022-04-29 14:38:15 -0300

    Removing redundant comment.

--- Exercise_1/main.c
+++ Exercise_1/main.c
@@ -13,8 +13,6 @@
 
 #define FILE_PATH "output.bin"
 
-//Programa por Maxsuel Fernandes de Almeida, de nUSP  e Rafael Pereira de Gouveia, de nUSP 11800820
-
 int main(void)
 {
     student_t **student;
