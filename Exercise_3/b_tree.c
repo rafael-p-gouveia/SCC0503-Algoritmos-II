b1bd57c7d3aefd89cdee31445c35f1504daf2893
rafael-p-gouveia <85884457+rafael-p-gouveia@users.noreply.github.com>
2022-06-03 21:32:36 -0300

    Delete Exercise_3 directory

--- Exercise_3/b_tree.c
+++
@@ -1,3 +0,0 @@
-#include "b_tree.h"
-
-#define ENTRY_NOT_FOUND -1
--- Exercise_3/b_tree.h
+++
@@ -1,7 +0,0 @@
-#ifndef B_TREE
-#define B_TREE
-
-long int search_bTree(int key);
-void add_b_tree(int key, long int RRN);
-
-#endif // B_TREE
