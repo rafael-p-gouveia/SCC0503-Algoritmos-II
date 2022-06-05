a6bf730f41b89a157cda9204462298e17808ae97
rafael-p-gouveia <rafael.p.gouveia2@gmail.com>
2022-06-05 15:43:46 -0300

    header atualizado

--- Exercise_3/b_tree.h
+++ Exercise_3/b_tree.h
@@ -4,6 +4,7 @@
 #define ENTRY_NOT_FOUND (long int)-1
 long int search_bTree(int key);
 long int recursion_search(int key, long int nodeRRN, FILE* bTree);
+int find_key_position(int key, int *arrayKeys, int numberOfKeys, int *isInNode);
 void add_b_tree(int key, long int RRN);
 void create_b_tree();
 
